1ce618ca92b17bc22b77a3bd3c0f97274a719c7f
MiiguelMc <mmacpin1111@g.educaand.es>
2024-10-03 21:05:10 +0200

    Subida de 4 Ejercicio

---
+++ JAVA/TEMA1/Ejercicio4/Ejercicio4/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("convertido de euros a pesetas " );
+        double euros = 550.36;
+        System.out.println("Aqui podemos ver las cantidad " + (euros * 166.38) );
+
+        
+
+    }
+}
