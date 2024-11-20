4a3085312ce06b79119eed0cfe12633ccd65d3a4
unknown <mmacpin@g.educaand.es>
2024-11-20 18:47:32 +0100

    Subida del Ejercicio de palotes

--- JAVA/TEMA3/Ejercicio7/src/App.java
+++ JAVA/TEMA3/Ejercicio7/src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Introduce un numero");
+        int numero = Integer.parseInt(System.console().readLine());
+        int  invertido = 0, resto;
+        while (numero > 0) {
+            resto = numero % 10;
+            invertido = invertido * 10 + resto;
+            numero /= 10;
+        }
+        while (invertido > 0) {
+            int alreves = (invertido % 10);
+            invertido = invertido / 10;
+            for ( int i = 0; i < alreves; i++) {
+                System.out.print("|");
+            }
+            if (invertido >= 1){
+                System.out.print("-");
+            }
+        }
     }
 }
